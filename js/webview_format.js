127a7131294c8616929dd86cf5cda31b40da1db0
FYNDevelopers <53409080+FYNDevelopers@users.noreply.github.com>
2022-02-14 18:50:00 +0300

    Update webview_format.js

--- js/webview_format.js
+++ js/webview_format.js
@@ -1,1 +1,19 @@
-/** News**/var news_header = document.getElementsByClassName("site-footer-wrap"); var news_footer = document.getElementsByClassName('site-mobile-header-wrap');or (var i = 0; i < news_header.length; i ++) {     news_header[i].style.display = 'none'; }for (var i = 0; i < news_footer.length; i ++) {     news_footer[i].style.display = 'none'; }/** Podcasts **/var appBanners = document.getElementsByClassName('bbQTfD');var appBanners = document.getElementsByClassName('gbtPqP'); r (var i = 0; i < appBanners.length; i ++) {     appBanners[i].style.display = 'none'; } for (var i = 0; i < appBanners.length; i ++) {     appBanners[i].style.display = 'none'; }
+/** News**/
+var news_header = document.getElementsByClassName("site-footer-wrap"); 
+var news_footer = document.getElementsByClassName('site-mobile-header-wrap');
+
+for (var i = 0; i < news_header.length; i ++) {
+     news_header[i].style.display = 'none'; };
+
+for (var i = 0; i < news_footer.length; i ++) {
+     news_footer[i].style.display = 'none'; };
+
+/** Podcasts **/
+var appBanners = document.getElementsByClassName('bbQTfD');
+var appBanners = document.getElementsByClassName('gbtPqP'); 
+
+for (var i = 0; i < appBanners.length; i ++) {
+     appBanners[i].style.display = 'none'; };
+ 
+for (var i = 0; i < appBanners.length; i ++) {
+     appBanners[i].style.display = 'none'; };
