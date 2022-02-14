9f7647e1923561118223c0d39c8c8965e2534326
FYNDevelopers <53409080+FYNDevelopers@users.noreply.github.com>
2022-02-14 18:27:13 +0300

    Update webview_format.js

--- js/webview_format.js
+++ js/webview_format.js
@@ -1,3 +1,4 @@
+/** News**/
 var news_header = document.getElementsByClassName("site-footer-wrap"); 
 var news_footer = document.getElementsByClassName('site-mobile-header-wrap');
 
@@ -6,3 +7,13 @@ for (var i = 0; i < news_header.length; i ++) {
 
 for (var i = 0; i < news_footer.length; i ++) {
      news_footer[i].style.display = 'none'; }
+
+/** Podcasts **/
+var appBanners = document.getElementsByClassName('bbQTfD');
+var appBanners = document.getElementsByClassName('gbtPqP'); 
+
+for (var i = 0; i < appBanners.length; i ++) {
+     appBanners[i].style.display = 'none'; }
+ 
+for (var i = 0; i < appBanners.length; i ++) {
+     appBanners[i].style.display = 'none'; }
