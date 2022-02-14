2ea0596bf9483b497f20bac8f42e61e2b4fb75bd
FYNDevelopers <53409080+FYNDevelopers@users.noreply.github.com>
2022-02-14 18:09:27 +0300

    Create news.js

---
+++ js/news.js
@@ -0,0 +1,8 @@
+var news_header = document.getElementsByClassName("site-footer-wrap"); 
+var news_footer = document.getElementsByClassName('site-mobile-header-wrap');
+
+for (var i = 0; i < news_header.length; i ++) {
+     news_header[i].style.display = 'none'; }
+
+for (var i = 0; i < news_footer.length; i ++) {
+     news_footer[i].style.display = 'none'; }
